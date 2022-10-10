3bae09e599141af5c3a09ddb5ff92a7a38c952e0
Solomon <Solomonaz2022@gmail.com>
2022-10-10 05:38:38 -0700

    Big o

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,44 +1,37 @@
 #include "sort.h"
+
 /**
- * shell_sort - function that sorts an array
- * of integers in ascending order
- * @array: input arrray
- * @size: size of the array
- * Return: no return
- */
+  * shell_sort - A function tat sorts an array usin shell algorithm.
+  * @array: The array to sort.
+  * @size: The length of the array.
+  * Return: Nothing.
+  */
 void shell_sort(int *array, size_t size)
 {
-	size_t n = 1;
+	unsigned int i = 0, j = 0, gap = 0;
+	int aux = 0;
 
-	while (n < size)
-		n = (n * 3) + 1;
+	if (array == NULL || size < 2)
+		return;
 
-	while ((n = (n - 1) / 3) > 0)
-		_ssort(array, size, n);
-}
-/**
- * _ssort - auxiliar function for
- * shell_sort function
- * @a: input arrray
- * @size: size of the array
- * @n: intervale
- * Return: no return
- */
-void _ssort(int *a, int size, int n)
-{
-	int tmp, i, j;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-	for (i = 0; (i + n) < size; i++)
+	for (; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (j = i + n; (j - n) >= 0; j = j - n)
+		for (i = gap; i < size; i++)
 		{
-			if (a[j] < a[j - n])
+			aux = array[i];
+			for (j = i; j >= gap && array[j - gap] > aux;
+					j -= gap)
 			{
-				tmp = a[j];
-				a[j] = a[j - n];
-				a[j - n] = tmp;
+				if (array[j] != array[j - gap])
+					array[j] = array[j - gap];
 			}
+			if (array[j] != aux)
+				array[j] = aux;
+
 		}
+		print_array(array, size);
 	}
-	print_array(a, size);
 }
